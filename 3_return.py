9517186094caebf7aa2bd36862811b17ac633d06
bdmorales5 <bdmorales5@cps.edu>
2026-01-26 13:00:20 -0600

    Update 3_return.py

--- 3_return.py
+++ 3_return.py
@@ -4,24 +4,15 @@
 
 
 
-
-
+# Return Practice #1
+def power(base, exponent):
+    return base ** exponent
 
 # Return Practice #2
-# Create a function called usd_to_eur that takes a numeric value (an amount in US dollars) as its only parameter, and returns the equivalent amount in euros as a result. For the purposes of this example, we will take the conversion 1 USD = 0.90 EUR.
+def usd_to_eur(dollars):
+    return dollars * 0.90
 
-# Create a variable called dollars and store any amount in it. Then, pass it to your function and evaluate its result.
-
-# Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
-
-
+dollars = 50
+result_eur = usd_to_eur(dollars)
 
 
-# Return Practice #3
-# Create a function called reverse_word that takes the characters of a given word as an argument, reverses the order of their characters, and returns them that way and in uppercase.
-
-# For example, if we pass it the word "Python", it should return: "NOHTYP"
-
-# Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
-
-# Hint: inside the created function, you should use string methods already seen.
