fbcea268bf54d0a98105f74892e07fff3599dae3
bdmorales5 <bdmorales5@cps.edu>
2026-01-26 12:48:58 -0600

    Update 1_.py

--- 1_.py
+++ 1_.py
@@ -1,34 +1,63 @@
-# Methods, Help & Documentation Practice #1
-# Remove the characters to the left of our main text:
+# function = a block of reusable code
+#             place()after the function name to invoke it
 
-# ,
-
-# :
-
-# %
-
-# _
-
-# #
-
-# Use the lstrip() method. Print the result to the screen:
-
-# ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
-
-# Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
 
 
-# Methods, Help & Documentation Practice #2
-# Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
 
-# fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
 
-# Search the documentation for the requested method to know how it works.
+# print("Happy birhtday to you!")
+# print("You are old")
+# print("Happy birthday to you!")
 
-# Methods, Help & Documentation Practice #3
-# Check if the sets below are isolated (that is, they have no elements in common), using the isdisjoint() method. Store this result in the isolated_sets variable:
 
-# phone_brands = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
-# tv_brands = {"Sony", "Philips", "Samsung", "LG"}
-# Search the documentation for the requested method to know how it works.
+# def happy_birthday(name, uge):
+#     print(f"Happy  Birhtday to {name}!")
+# print(f"You are {uge} years old")
+# print("Happy birthday to you!")
+
+
+# happy_birthday("bro, 20")
+# happy_birthday("Stave, 35")
+# happy_birthday("Joe, 25")
+
+# def display_invoice(username, amount , due_date):
+#     print(F"hello {username}")
+#     print(F"your bill of ${amount:.2f} is due" {due_date}")
+          
+# display_invoice("joeSchom", 100.01, "01/02")
+
+# def add(x,y):
+#     z= x + y
+#     return z 
+
+# def subtract(x, y):
+#     z = x - y 
+#     return z 
+
+# def multiplay(x , y ):
+#     z = x * y 
+#     return z 
+
+# def division(x , y):
+#     z = x / y
+#     return z 
+
+# print(add(1 , 2))
+# print(subtract(1, 2))
+# print(multiplay(1 ,2))
+# print(division(1, 2))
+
+def create_name(first , last):
+    first= first.capitalize()
+    last=last.capitalize()
+    return first +" "+ last 
+full_name= create_name("bro", "code")
+
+print(full_name)
 
