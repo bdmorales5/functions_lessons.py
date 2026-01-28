f1d0210cf862c9ae2ef26747d2c8bbe975e186db
bdmorales5 <bdmorales5@cps.edu>
2026-01-28 13:27:40 -0600

    Implement number_attributes and list_attributes functions
    Added functions to count and list keyword arguments.

--- 8_indefinite_arguments_kwargs.py
+++ 8_indefinite_arguments_kwargs.py
@@ -2,6 +2,14 @@
 # Indefinite Arguments (**kwargs) Practice #1
 # Create a function called number_attributes that counts the number of parameters that are passed, and returns that number as the result.
 
+def number_attributes(**kwargs):
+    count = 0
+    for key, value in kwargs.items():
+        count += 1
+        #first time through: count = 0 + 1 = 1
+        #second time through: count = 1 + 1 = 2
+        #third time through: count = 2 + 1 = 3
+    return count 
 
 
 
@@ -15,26 +23,14 @@
 
 
 
+def list_attributes(**kwargs):
+    values_list = []
+    for key, value in kwargs.items():
+        values_list.append(value)
+        #first time through: values_list = [] + [blue] = [blue]
+        #second time through: values_list = [blue] + [tall] = [blue, tall]
+        #third time through: values_list = [blue, tall] + [smart] = [blue, tall, smart]
+    return values_list
+print(list_attributes(eye_color="blue", height="tall", intelligence="smart"))
 
 
-
-
-
-
-
-# Indefinite Arguments (**kwargs) Practice #3
-# Create a function called describe_person, which takes his name as parameters and then an indeterminate number of arguments. This function should display on the screen:
-
-# Characteristics of {name}:
-# {argument_name}: {argument_value}
-# {argument_name}: {argument_value}
-# etc...
-# For example:
-
-# describe_person("Ash", eye_color="brown", hair_color="black")
-
-# Will print to the screen:
-
-# Characteristics of Ash:
-# eye_color: brown
-# hair_color: black
