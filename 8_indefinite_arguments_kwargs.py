43c41e619f56bfef81d5ee5737bc21c4533b3933
bdmorales5 <bdmorales5@cps.edu>
2026-01-28 13:22:45 -0600

    Implement number_attributes and list_attributes functions
    Added functions to count and list keyword arguments.

--- 8_indefinite_arguments_kwargs.py
+++ 8_indefinite_arguments_kwargs.py
@@ -2,6 +2,14 @@
 # Indefinite Arguments (**kwargs) Practice #1
 # Create a function called number_attributes that counts the number of parameters that are passed, and returns that number as the result.
 
+def number_attributes(**kwargs):
+    count = 0
+    for key, value in kwargs.items():
+        count += 1
+        #first time through: count = 0 + 1 = 1
+        #second time through: count = 1 + 1 = 2
+        #third time through: count = 2 + 1 = 3
+    return count 
 
 
 
@@ -15,10 +23,15 @@
 
 
 
-
-
-
-
+def list_attributes(**kwargs):
+    values_list = []
+    for key, value in kwargs.items():
+        values_list.append(value)
+        #first time through: values_list = [] + [blue] = [blue]
+        #second time through: values_list = [blue] + [tall] = [blue, tall]
+        #third time through: values_list = [blue, tall] + [smart] = [blue, tall, smart]
+    return values_list
+print(list_attributes(eye_color="blue", height="tall", intelligence="smart"))
 
 
 
